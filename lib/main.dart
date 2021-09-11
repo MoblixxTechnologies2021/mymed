d450c90bbbebb55a17484be9eb39d4d2f10f6e00
adeelghumman <adeeljaved839@gmail.com>
2021-09-11 13:44:18 +0500

    adeel Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:mymed/View/Initalsetup.dart';
 import 'package:mymed/View/uniqueID.dart';
-=======
 import 'package:mymed/View/Reconcile.dart';
 import 'package:get/get.dart';
->>>>>>> 4c6e1f8f8688cf34051b7f466441e844cbafc472
 
 void main() {
   runApp(MyApp());
@@ -19,12 +16,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter emo',
       theme: ThemeData(
-<<<<<<< HEAD
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: UniqueID(),
-=======
         // This is the theme of your application.
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
@@ -35,8 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: ReconcilScreen(),
->>>>>>> 4c6e1f8f8688cf34051b7f466441e844cbafc472
+      home: UniqueID(),
     );
   }
 }
